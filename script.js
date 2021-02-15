cf7c387a5073ba116c983375cf22078cf10b3bab
mannuluss <49813014+mannuluss@users.noreply.github.com>
2021-02-15 17:48:13 -0500

    FINALIZADO
    se hizo todo

---
+++ script.js
@@ -0,0 +1,38 @@
+var list;
+$(document).ready(function () {
+    var url = "https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json"
+    $.ajax({
+        type: "GET",
+        url: url,
+        data: "",
+        dataType: "json",
+        success: function (response) {
+            var i =0;
+            var select = $('#departamento');
+            list = response;
+
+            response.forEach(element => {
+                console.log(element['ciudad']);
+                var str = '<option value="'+element['departamento']+'">'+element['departamento']+'</option>';
+                select.append(str);
+                i++;
+            });
+        }
+    });
+});
+
+function country(){  
+    var depaName = $('#departamento').val();
+    var city = $('#city');
+    console.log(list);
+    city.empty();
+    list.forEach(element =>{
+        if(element['departamento'] == depaName){
+            console.log(element['ciudades']);
+            element['ciudades'].forEach(ciudad =>{
+                var str = '<option value="' + ciudad + '">'+ ciudad +'</option>';
+                city.append(str);
+            });
+        }
+    });
+}
